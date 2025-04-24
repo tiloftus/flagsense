56d660a9ab62e330274cd425ceb349f3de7ad329
Timothy Loftus <tiloftus.4@gmail.com>
2025-04-24 11:00:16 -0400

    added nigeria image

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
 	name="flagsense",
-	version="0.1",
+	version="0.1.0",
+    description="A Python package for flag detection and classification in images, leveraging deep learning models for efficient and accurate results.",
+    author="Timothy Loftus",
+    author_email="tiloftus.4@gmail.com",
+    url="https://github.com/tiloftus/flagsense",
 	packages=find_packages(include=["flagsense", "flagsense.*"]),
 	install_requires=[
 		"ultralytics",
